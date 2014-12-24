1c4a78f07a85681b54445b086bb7a2ef1d341a82
simon_gl <wudag_7318@163.com>
2014-12-24 12:00:32 +0800

    v1.1.201224 IM项目源码

---
+++ src/EntboostIM/src/com/entboost/im/contact/DefaultUserInfoActivity.java
@@ -0,0 +1,75 @@
+package com.entboost.im.contact;
+
+import net.yunim.service.EntboostCache;
+import net.yunim.service.entity.CardInfo;
+import net.yunim.service.entity.MemberInfo;
+import net.yunim.utils.ResourceUtils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.entboost.im.R;
+import com.entboost.im.base.EbActivity;
+import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+public class DefaultUserInfoActivity extends EbActivity {
+	@ViewInject(R.id.user_head)
+	private ImageView userHead;
+	@ViewInject(R.id.na)
+	private TextView na;
+	@ViewInject(R.id.ph)
+	private TextView ph;
+	@ViewInject(R.id.tel)
+	private TextView tel;
+	@ViewInject(R.id.em)
+	private TextView em;
+	@ViewInject(R.id.ti)
+	private TextView ti;
+	@ViewInject(R.id.de)
+	private TextView de;
+	@ViewInject(R.id.en)
+	private TextView en;
+	@ViewInject(R.id.ad)
+	private TextView ad;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setAbContentView(R.layout.activity_default_user_info);
+		ViewUtils.inject(this);
+		Long uid = getIntent().getLongExtra("uid", -1);
+		if (uid > 0) {
+			CardInfo card = EntboostCache.getCardInfo(uid);
+			if (card != null) {
+				if(StringUtils.isNotBlank(card.getEc())){
+					Long mid=Long.valueOf(card.getEc());
+					MemberInfo member = EntboostCache.getMemberByCode(mid);
+					if(member!=null){
+						Bitmap img = ResourceUtils.getHeadBitmap(member.getH_r_id());
+						if (img != null) {
+							userHead.setImageBitmap(img);
+						} else {
+							userHead.setImageResource(R.drawable.head1);
+							new BitmapUtils(this).display(userHead, member.getHeadUrl());
+						}
+					}
+				}
+				na.setText(card.getNa());
+				ph.setText(card.getPh());
+				tel.setText(card.getTel());
+				em.setText(card.getEm());
+				ti.setText(card.getTi());
+				de.setText(card.getDe());
+				en.setText(card.getEn());
+				ad.setText(card.getAd());
+			}
+		}
+	}
+
+}
